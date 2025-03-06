3841aa0989aaf8cef179eae3a3486be394835441
Vihenka228 <161455757+Vihenka228@users.noreply.github.com>
2025-03-06 12:50:19 +0300

    feat: add client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from 'react-router-dom';
+import Header from './Components/Header/header';
+import { checkAuth } from "./store/slice/authSlice"; 
+import Loading from "./Components/Loading/loading";
+import Main from './pages/Main/Main';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const dispatch = useDispatch();
+    const loading = useSelector(state => state.auth.isLoading);
+    const user = useSelector(state => state.auth.user); 
+    const [isInitialized, setIsInitialized] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            dispatch(checkAuth()).finally(() => {
+                setIsInitialized(true);
+            });
+        } else {
+            setIsInitialized(true);
+        }
+    }, [dispatch]);
+
+    if (!isInitialized) {
+        return <Loading />;
+    }
+
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                </Routes>
+            </main>
+        </div>
+    );
+};
 
 export default App;
---
+++ client/src/http/apiEndpoints.js
@@ -0,0 +1,5 @@
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+export const API_ENDPOINTS = {
+
+};
